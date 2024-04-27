6695c970cce6e3c9f8a04a2b4f5a88a02d6474aa
41047036s <93424551+41047036s@users.noreply.github.com>
2024-04-27 23:36:11 +0800

    imgPath

--- GUI/mainwindow.py
+++ GUI/mainwindow.py
@@ -60,7 +60,7 @@ class Window(QMainWindow, Ui_MainWindow):
 
     @pyqtSlot()
     def on_openAct_triggered(self):
-        imgPath = r"C:\Users\USER\Documents\GitHub\Image_processing\data\1.bmp"
+        imgPath = r"C:\Users\88696\Documents\GitHub\Image_processing\data\1.bmp"
         #imgPath = QFileDialog.getOpenFileName(
         #    self, "请选择图片", "/", "All Files (*)")[0]
         self.openImage(imgPath)
